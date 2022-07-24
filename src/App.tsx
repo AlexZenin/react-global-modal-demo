75ce80c592d31767b33860e5cf0a78a4854c8716
Alex Zenin <alexander.zenin@reece.com.au>
2022-07-24 18:14:50 +1000

    chore: format code

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline p-10">Vite + React Modal Demo</h1>
+      <h1 className="text-3xl font-bold underline p-10">
+        Vite + React Modal Demo
+      </h1>
     </div>
   )
 }
